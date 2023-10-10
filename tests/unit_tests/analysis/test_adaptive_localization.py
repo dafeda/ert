40a21e0546d65722534387bfadb2bcb4a17d010a
Anna Kvashchuk <kvashchuk.anna@gmail.com>
2023-10-10 15:53:51 +0200

    Add test for cut-off 0

--- tests/unit_tests/analysis/test_adaptive_localization.py
+++ tests/unit_tests/analysis/test_adaptive_localization.py
@@ -43,3 +43,56 @@ def test_that_adaptive_localization_with_cutoff_1_equals_ensemble_prior(copy_cas
     )
 
 
+def test_that_adaptive_localization_with_cutoff_0_equals_ESupdate(copy_case):
+    copy_case("poly_example")
+
+    parser = ArgumentParser(prog="test_main_0")
+    parsed_loc0 = ert_parser(
+        parser,
+        [
+            ENSEMBLE_SMOOTHER_MODE,
+            "--current-case",
+            "default_loc0",
+            "--target-case",
+            "target_loc0",
+            "--realizations",
+            "1-50",
+            "poly_loc_0.ert",
+            "--port-range",
+            "1024-65535",
+        ],
+    )
+
+    run_cli(parsed_loc0)
+    facade_loc0 = LibresFacade.from_config_file("poly_loc_0.ert")
+    with open_storage(facade_loc0.enspath) as storage_loc0:
+        target_fs_loc0 = storage_loc0.get_ensemble_by_name("target_loc0")
+        posterior_loc0 = facade_loc0.load_all_gen_kw_data(target_fs_loc0)
+
+    parser_noloc = ArgumentParser(prog="test_main")
+    parsed_noloc = ert_parser(
+        parser_noloc,
+        [
+            ENSEMBLE_SMOOTHER_MODE,
+            "--current-case",
+            "default_noloc",
+            "--target-case",
+            "target_noloc",
+            "--realizations",
+            "1-50",
+            "poly_no_loc.ert",
+            "--port-range",
+            "1024-65535",
+        ],
+    )
+
+    run_cli(parsed_noloc)
+    facade_noloc = LibresFacade.from_config_file("poly.ert")
+    with open_storage(facade_noloc.enspath) as storage_noloc:
+        target_fs_noloc = storage_noloc.get_ensemble_by_name("target_noloc")
+        posterior_noloc = facade_noloc.load_all_gen_kw_data(target_fs_noloc)
+
+    # We expect that when cut-off is 0, the posterior is equal to ES
+    assert (
+        np.linalg.det(posterior_loc0.cov().to_numpy()) == np.linalg.det(posterior_noloc.cov().to_numpy())
+    )
