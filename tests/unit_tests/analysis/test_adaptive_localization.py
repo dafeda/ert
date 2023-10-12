0a672e7f7b6ad99a0131904981a3875519670e42
Anna Kvashchuk <kvashchuk.anna@gmail.com>
2023-10-12 14:03:39 +0200

    Refactor out Libres Facade

--- tests/unit_tests/analysis/test_adaptive_localization.py
+++ tests/unit_tests/analysis/test_adaptive_localization.py
@@ -1,40 +1,44 @@
 from argparse import ArgumentParser
+from textwrap import dedent
 
 import numpy as np
 import pytest
 
-from ert import LibresFacade
 from ert.__main__ import ert_parser
 from ert.cli import ENSEMBLE_SMOOTHER_MODE
 from ert.cli.main import run_cli
 from ert.storage import open_storage
 
+from src.ert.config import ErtConfig
 
-def run_cli_ES_with_case(ert_config):
+
+def run_cli_ES_with_case(poly_config):
     parser = ArgumentParser(prog="test_main")
     parsed = ert_parser(
         parser,
         [
             ENSEMBLE_SMOOTHER_MODE,
             "--current-case",
-            "prior_sample",
+            "prior_sample"+poly_config,
             "--target-case",
-            "posterior_sample",
+            "posterior_sample"+poly_config,
             "--realizations",
             "1-50",
-            ert_config,
+            poly_config,
             "--port-range",
             "1024-65535",
         ],
     )
 
     run_cli(parsed)
-    facade = LibresFacade.from_config_file(ert_config)
-    with open_storage(facade.enspath) as storage:
-        prior_ensemble_name = storage.get_ensemble_by_name("prior_sample")
-        prior_sample = facade.load_all_gen_kw_data(prior_ensemble_name)
-        posterior_ensemble_name = storage.get_ensemble_by_name("posterior_sample")
-        posterior_sample = facade.load_all_gen_kw_data(posterior_ensemble_name)
+    storage_path = ErtConfig.from_file(poly_config).ens_path
+
+    #facade = LibresFacade.from_config_file(ert_config)
+    with open_storage(storage_path) as storage:
+        prior_ensemble = storage.get_ensemble_by_name("prior_sample")
+        prior_sample = prior_ensemble.load_parameters("COEFFS")
+        posterior_ensemble = storage.get_ensemble_by_name("posterior_sample")
+        posterior_sample = posterior_ensemble.load_parameters("COEFFS")
 
     return prior_sample, posterior_sample
 
@@ -42,8 +46,22 @@ def run_cli_ES_with_case(ert_config):
 @pytest.mark.integration_test
 def test_that_adaptive_localization_with_cutoff_1_equals_ensemble_prior(copy_case):
     copy_case("poly_example")
-
-    prior_sample, posterior_sample = run_cli_ES_with_case("poly_loc_1.ert")
+    # random_seed_line = "RANDOM_SEED 1234"
+    # set_adaptive_localization = dedent(
+    #     """
+    #     ANALYSIS_SET_VAR STD_ENKF LOCALIZATION True
+    #     ANALYSIS_SET_VAR STD_ENKF LOCALIZATION_CORRELATION_THRESHOLD 1.0
+    #     """
+    # )
+    #
+    # with ("poly.ert", "r+") as f:
+    #     lines = f.readlines()
+    #     lines.insert(3, random_seed_line)
+    #     lines.insert(10, set_adaptive_localization)
+    #
+    # with open("poly_loc_1.ert", "w") as f:
+    #     f.writelines(lines)
+    prior_sample, posterior_sample = run_cli_ES_with_case("poly.ert")
 
     # Check prior and posterior samples are equal
     assert np.array_equal(posterior_sample, prior_sample)
