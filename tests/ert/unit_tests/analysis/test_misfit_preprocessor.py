3daa98967d0a6ad0b8aec69d822349ac2d124704
lucaeg <lgaz@equinor.com>
2026-02-12 10:21:21 +0100

    typos

--- tests/ert/unit_tests/analysis/test_misfit_preprocessor.py
+++ tests/ert/unit_tests/analysis/test_misfit_preprocessor.py
@@ -551,11 +551,11 @@ def test_independent_measurments_clustered_together_in_case_of_irregular_obs_err
     observations are clustered together, and treated as correlated.
 
     Scenario:
-    Suppose we have 100 independend observations r_1,r_2,...,r_100 with corresponding
+    Suppose we have 100 independent observations r_1,r_2,...,r_100 with corresponding
     observation errors, where r_1 has a small error, and r_2,...,r_100 have large
     errors. The error-scaling step amplifies r_1's response and suppresses
     r_2,...,r_100, leading to one cluster instead of 100 clusters. As a result, the
-    history matching update is defalted as if all 100 observations were
+    history matching update is deflated as if all 100 observations were
     perfectly correlated, even though they are all independent.
     """
 
